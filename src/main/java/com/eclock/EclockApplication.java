932b8f27619166614edc12385ed28f7be9452a4d
Arpan Subedi <arpansubedi@gmail.com>
2025-02-19 16:49:08 +1300

    eclock project for 159333 initialised

---
+++ src/main/java/com/eclock/EclockApplication.java
@@ -0,0 +1,13 @@
+package com.eclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eclock/EclockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
